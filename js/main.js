4ef9cc0df7684f35270d9fa15983bce76e74d8fb
Mahesa <mahesaagsa101@gmail.com>
2026-02-14 15:30:21 +0700

    menambahkan fitur menerima pesa lewat gmail

--- js/main.js
+++ js/main.js
@@ -39,8 +39,34 @@ window.addEventListener('scroll', shadowHeader)
 
 
 /*=============== EMAIL JS ===============*/
+const contactForm =document.getElementById('contact-form'),
+contactMessage =document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+   e.preventDefault()
+
+   // serviceID - templateID - #form - publicKey
+   emailjs.sendForm('service_101hesa3','template_2t9grc8','#contact-form','Kx9ItM64ue9u2n66G')
+   .then(()=>{
+     // Show sent message
+
+     contactMessage.textContent = 'Message sent successfully ✅'
+
+     // Remove message after five seconds
+      setTimeout(() =>{
+         contactMessage.textContent =''
+      },5000)
+
+   // Clear input fields
+    contactForm.reset()
+   },()=>{
+      // Show error message
+      contactMessage.textContent = 'Message not sent (service error) ❌'
+   }) 
+}
 
 
+contactForm.addEventListener('submit',sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
 
 
